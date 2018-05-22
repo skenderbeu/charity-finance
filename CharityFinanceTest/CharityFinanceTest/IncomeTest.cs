0c44995fc418f8a101bdde2881c5293d58c0b9e2
Skenderbeu <pc2984@gmail.com>
2018-05-22 21:55:37 +0100

    Added Income class and test for adding Income

--- CharityFinanceTest/CharityFinanceTest/IncomeTest.cs
+++ CharityFinanceTest/CharityFinanceTest/IncomeTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FinanceEntities;
 
 namespace CharityFinanceTests
 {
@@ -7,8 +8,23 @@ namespace CharityFinanceTests
     public class IncomeTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Income_AddIncome()
         {
+            Income income = new Income{
+                Date = DateTime.Parse("22/05/2018"),
+                Description = "Offering 20/05/18",
+                PaymentType = PaymentTypes.CASH,
+                PayingInSlip = "000124",
+                Amount = 230.00,
+                GiftAidStatus = GiftAidStatus.NotGiftAid,
+                BudgetType = BudgetTypes.GeneralIncome };
+
+
+            var expected = 230.00;
+                
+            var actual = income.Amount;
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
--- CharityFinanceTest/Entities/Income.cs
+++ CharityFinanceTest/Entities/Income.cs
@@ -8,5 +8,31 @@ namespace FinanceEntities
 {
     public class Income
     {
+        public DateTime Date{ get; set; }
+        public string Description { get; set; }
+        public PaymentTypes PaymentType { get; set; }
+        public string PayingInSlip { get; set; }
+        public Double Amount { get; set; }
+        public BudgetTypes BudgetType { get; set; }
+        public GiftAidStatus GiftAidStatus { get; set; }
+    }
+
+    public enum PaymentTypes
+    {
+        CHQ,
+        BAC,
+        SO,
+        CASH
+    }
+
+    public enum BudgetTypes
+    {
+        GeneralIncome
+    }
+
+    public enum GiftAidStatus
+    {
+        NotGiftAid,
+        GiftAid
     }
 }
