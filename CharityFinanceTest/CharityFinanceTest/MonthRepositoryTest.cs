9358cfd5ed4163670bad2c7630938a8f6f95b776
Peter Campbell <pc2984@hotmail.co.uk>
2018-07-26 20:45:53 +0100

    Added tests for MonthRepo , and added some code to Month Repo

---
+++ CharityFinanceTest/CharityFinanceTest/MonthRepositoryTest.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Repositories;
+using FinanceEntities;
+
+namespace CharityFinanceTest
+{
+    [TestClass]
+    public class MonthRepositoryTests
+    {
+
+        IMonthRepository repo;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            repo = new MonthRepository(MockPayments.Payments(), MockIncomes.Incomes());
+        }
+
+        [TestCleanup]
+        public void CloseDown()
+        {
+
+            repo = null;
+        }
+
+        [TestMethod]
+        public void GetPaymentsTotalByMonth_June2018_Returns200()
+        {
+            //Arrange
+            var month = Month.June;
+            var year = 2018;
+
+            //Act
+            var actual = repo.GetPaymentsTotalByMonth(month, year);
+            var expected = 200.00;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetPaymentsTotalByMonth_May2018_Returns0()
+        {
+            //Arrange
+            var month = Month.May;
+            var year = 2018;
+
+            //Act
+            var actual = repo.GetPaymentsTotalByMonth(month, year);
+            var expected = 0.00;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException (typeof(ArgumentException), "Month has not been set properly")]
+        public void GetPaymentsTotalByMonth_NotSet2018_throwsException()
+        {
+            //Arrange
+            var month = Month.NotSet;
+            var year = 2018;
+
+            //Act
+            var actual = repo.GetPaymentsTotalByMonth(month, year);
+        }
+
+        [TestMethod]
+        public void GetIncomeTotalByMonth_June2018_Returns0()
+        {
+            //Arrange
+            var month = Month.June;
+            var year = 2018;
+
+            //Act
+            var actual = repo.GetIncomeTotalByMonth(month, year);
+            var expected = 0.00;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetIncomeTotalByMonth_May2018_Returns400()
+        {
+            //Arrange
+            var month = Month.May;
+            var year = 2018;
+
+            //Act
+            var actual = repo.GetIncomeTotalByMonth(month, year);
+            var expected = 500.00;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Month has not been set properly")]
+        public void GetIncomeTotalByMonth_NotSet2018_throwsException()
+        {
+            //Arrange
+            var month = Month.NotSet;
+            var year = 2018;
+
+            //Act
+            var actual = repo.GetIncomeTotalByMonth(month, year);
+        }
+
+    }
+}
--- CharityFinanceTest/Repositories/MonthRepository.cs
+++ CharityFinanceTest/Repositories/MonthRepository.cs
@@ -1,12 +1,14 @@
 ﻿using FinanceEntities;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Repositories
 {
     public interface IMonthRepository
     {
         double GetPaymentsTotalByMonth(Month month, int year);
+        double GetIncomeTotalByMonth(Month month, int year);
         double GetBankedClearedPaymentsTotalByMonth(Month month, int year);
         double GetOSChequesByMonth(Month month, int year);
     }
@@ -29,7 +31,20 @@ namespace Repositories
 
         public double GetPaymentsTotalByMonth(Month month, int year)
         {
-            throw new NotImplementedException();
+            if (month == Month.NotSet) { throw new ArgumentException("Month has not been set properly"); }
+
+            return payments
+                .Where(p => p.Date.Month == (int)month && p.Date.Year == year)
+                .Sum(p => p.Amount);
+        }
+
+        public double GetIncomeTotalByMonth(Month month, int year)
+        {
+            if (month == Month.NotSet) { throw new ArgumentException("Month has not been set properly"); }
+
+            return incomes
+                .Where(i => i.Date.Month == (int)month && i.Date.Year == year)
+                .Sum(i => i.Amount);
         }
 
         public double GetBankedClearedPaymentsTotalByMonth(Month month, int year)
