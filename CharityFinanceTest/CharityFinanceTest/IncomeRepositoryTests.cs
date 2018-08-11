3949fd4caaef8d039b9cbe9d0dec82c463c80f54
Peter Campbell <pc2984@hotmail.co.uk>
2018-08-11 16:11:51 +0100

    Added desktop View, added payment Repo and added fianance services

---
+++ CharityFinanceTest/CharityFinanceTest/IncomeRepositoryTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Repositories;
+using System.Linq;
+
+namespace CharityFinanceTests
+{
+    [TestClass]
+    public class IncomeRepositoryTests
+    {
+        IIncomeRepository repo;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            repo = new IncomeRepository(MockIncomes.Incomes());
+        }
+
+        [TestCleanup]
+        public void CloseDown()
+        {
+            repo = null;
+        }
+
+        [TestMethod]
+        public void GetIncomeByAmount_100_Returns3()
+        {
+            //Arrange
+            var amount = 100.00;
+
+            //Act
+            var actual = repo.GetIncomeByAmount(amount).ToList().Count();
+            var expected = 3;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetPaymentsByAmountWithDate_100_Returns2()
+        {
+            //Arrange
+            var amount = 100.00;
+            var dateOfPayment = DateTime.Parse("22/05/2018");
+
+            var payments = repo.GetIncomeByDate(dateOfPayment);
+
+            //Act
+            var actual = repo.GetIncomeByAmount(amount, payments).ToList().Count();
+            var expected = 2;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ CharityFinanceTest/Repositories/IncomeRepository.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FinanceEntities;
+
+namespace Repositories
+{
+    public interface IIncomeRepository : IDisposable
+    {
+        IEnumerable<Income> GetIncomeByDescription(string description);
+        IEnumerable<Income> GetIncomeByDescription(string description, IEnumerable<Income> incomeToFilterBy);
+        IEnumerable<Income> GetIncomeByDate(DateTime date);
+        IEnumerable<Income> GetIncomeByDateRange(DateTime dateFrom, DateTime dateTo);
+        IEnumerable<Income> GetIncomeByBudgetType(BudgetTypes budgetType);
+        IEnumerable<Income> GetIncomeByBudgetType(BudgetTypes budgetType, IEnumerable<Income> incomeToFilterBy);
+        IEnumerable<Income> GetIncomeByPaymentType(PaymentTypes paymentType);
+        IEnumerable<Income> GetIncomeByPaymentType(PaymentTypes paymentType, IEnumerable<Income> incomeToFilterBy);
+        IEnumerable<Income> GetIncomeByAmount(double amount);
+        IEnumerable<Income> GetIncomeByAmount(double amount, IEnumerable<Income> incomeToFilterBy);
+    }
+
+    public class IncomeRepository : IIncomeRepository
+    {
+        IEnumerable<Income> incomes;
+
+        public IncomeRepository()
+        {
+
+        }
+
+        //Used for Testing
+        public IncomeRepository(IEnumerable<Income> incomes)
+        {
+            this.incomes = incomes;
+        }
+
+        public IEnumerable<Income> GetIncomeByDescription(string description)
+        {
+            return incomes.Where(i => i.Description == description);
+        }
+
+        public IEnumerable<Income> GetIncomeByDescription(string description, IEnumerable<Income> incomeToFilterBy)
+        {
+           return incomeToFilterBy.Where(i => i.Description == description);
+        }
+
+        public IEnumerable<Income> GetIncomeByDate(DateTime date)
+        {
+            return incomes.Where(i => i.Date.Date == date.Date);
+        }
+
+        public IEnumerable<Income> GetIncomeByDateRange(DateTime dateFrom, DateTime dateTo)
+        {
+            return incomes.Where(i => i.Date.Date >= dateFrom && i.Date.Date <= dateTo);
+        }
+
+        public IEnumerable<Income> GetIncomeByBudgetType(BudgetTypes budgetType)
+        {
+            return incomes.Where(i => i.BudgetType == budgetType);
+        }
+
+        public IEnumerable<Income> GetIncomeByBudgetType(BudgetTypes budgetType, IEnumerable<Income> incomeToFilterBy)
+        {
+            return incomeToFilterBy.Where(i => i.BudgetType == budgetType);
+        }
+
+        public IEnumerable<Income> GetIncomeByPaymentType(PaymentTypes paymentType)
+        {
+            return incomes.Where(i => i.PaymentType == paymentType);
+        }
+
+        public IEnumerable<Income> GetIncomeByPaymentType(PaymentTypes paymentType, IEnumerable<Income> incomeToFilterBy)
+        {
+            return incomeToFilterBy.Where(i => i.PaymentType == paymentType);
+        }
+
+        public IEnumerable<Income> GetIncomeByAmount(double amount)
+        {
+            return incomes.Where(i => i.Amount == amount);
+        }
+
+        public IEnumerable<Income> GetIncomeByAmount(double amount, IEnumerable<Income> incomeToFilterBy)
+        {
+            return incomeToFilterBy.Where(i => i.Amount == amount);
+        }
+
+
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    incomes = null;
+                }
+
+                disposedValue = true;
+            }
+        }
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+
+        }
+
+        #endregion
+    }
+}
